12d326d78f70db1724c4610c5233ca3d69ea498a
LHJ Piper <2940596912@qq.com>
2025-02-22 23:57:48 +0800

    First try

--- src/extension.ts
+++ src/extension.ts
@@ -1,44 +1,57 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { AESGCMEncryption, AESGCMEncryptionConfigFromEnv, EncryptionService } from "easy-cipher-mate";
 
 export function activate(context: vscode.ExtensionContext) {
-    // 注册命令
-    let encryptDisposable = vscode.commands.registerCommand('easy-cipher-content.encrypt', () => {
-        const editor = vscode.window.activeTextEditor;
-        if (editor) {
-            const document = editor.document;
-            const text = document.getText();
-            const encrypted = text.toUpperCase();
-            
-            editor.edit(editBuilder => {
-                const range = new vscode.Range(
-                    document.positionAt(0),
-                    document.positionAt(text.length)
-                );
-                editBuilder.replace(range, encrypted);
-            });
-        }
-    });
+  const config = vscode.workspace.getConfiguration('easy-cipher-content');
+  const password = config.get('password');
 
-    let decryptDisposable = vscode.commands.registerCommand('easy-cipher-content.decrypt', () => {
-        const editor = vscode.window.activeTextEditor;
-        if (editor) {
-            const document = editor.document;
-            const text = document.getText();
-            const decrypted = text.toLowerCase();
-            
-            editor.edit(editBuilder => {
-                const range = new vscode.Range(
-                    document.positionAt(0),
-                    document.positionAt(text.length)
-                );
-                editBuilder.replace(range, decrypted);
-            });
-        }
-    });
+  const encryptionService = new EncryptionService(
+    new AESGCMEncryption(),
+    new AESGCMEncryptionConfigFromEnv(password as string),
+  );
 
-    // 添加订阅以确保资源在插件停用时释放
-    context.subscriptions.push(encryptDisposable);
-    context.subscriptions.push(decryptDisposable);
+  let encryptDisposable = vscode.commands.registerCommand(
+    "easy-cipher-content.encrypt",
+    async  () => {
+      const editor = vscode.window.activeTextEditor;
+      if (editor) {
+        const document = editor.document;
+        const text = document.getText();
+        const encrypted = await encryptionService.encryptText(text);
+
+        editor.edit((editBuilder) => {
+          const range = new vscode.Range(
+            document.positionAt(0),
+            document.positionAt(text.length)
+          );
+          editBuilder.replace(range, Buffer.from(encrypted.data).toString('utf-8'));
+        });
+      }
+    }
+  );
+
+  let decryptDisposable = vscode.commands.registerCommand(
+    "easy-cipher-content.decrypt",
+    () => {
+      const editor = vscode.window.activeTextEditor;
+      if (editor) {
+        const document = editor.document;
+        const text = document.getText();
+        const decrypted = text.toLowerCase();
+
+        editor.edit((editBuilder) => {
+          const range = new vscode.Range(
+            document.positionAt(0),
+            document.positionAt(text.length)
+          );
+          editBuilder.replace(range, decrypted);
+        });
+      }
+    }
+  );
+
+  context.subscriptions.push(encryptDisposable);
+  context.subscriptions.push(decryptDisposable);
 }
 
 export function deactivate() {}
